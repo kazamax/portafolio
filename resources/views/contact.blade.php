0912267c2dd47b89c42d60f799209507bfab647e
Roberto Figuera <roberto.figuera.web@gmail.com>
2017-02-19 13:28:26 -0400

    subo contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,115 +2,121 @@
 
 @section('content')
 
-<!-- 
-================================================== 
-    Global Page Section Start
-================================================== -->
-<section class="global-page-header">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="block">
-                    <h2>Contacto</h2>
-                    <ol class="breadcrumb">
-                        <li>
-                             <?php $inicio = 'https://web-kaz.herokuapp.com/'; ?>
-                            <a href="{{ $inicio }}">
-                                <i class="ion-ios-home"></i>
-                                Inicio
-                            </a>
-                        </li>
-                        <li class="active">Contacto</li>
-                    </ol>
+   <!-- 
+        ================================================== 
+            Global Page Section Start
+        ================================================== -->
+        <section class="global-page-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="block">
+                            <h2>Contacto</h2>
+                            <ol class="breadcrumb">
+                                <li>
+                                     <?php $inicio = 'https://web-kaz.herokuapp.com/'; ?>
+                                    <a href="{{ $inicio }}">
+                                        <i class="ion-ios-home"></i>
+                                        Inicio
+                                    </a>
+                                </li>
+                                <li class="active">Contacto</li>
+                            </ol>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>   
-</section><!--/#page-header-->
-<!-- 
-================================================== 
-    Contact Section Start
-================================================== -->
-<section id="contact-section">
-    <div class="container">
-        <div class="row">
-        @if(Session()->has('flash'))
-            <div class="alert alert-success alert-dismissible" role="alert">
-              <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-              {{ session()->get('flash' )}}
-          </div>
-        @endif
-            <div class="col-md-6">
-                <div class="block">
-                    <h2 class="subtitle wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".3s">Contacta conmigo</h2>
-                    <p class="subtitle-des wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".5s">
-                        Si tienes alguna recomendación, duda o sugerencia no dudes en escribirme, te respondere a la brevedad posible.
-                    </p>
-                    <div class="contact-form">
-                        <form id="contact-form" method="post" action="{{ route('messages') }}" role="form">
-                            {{ csrf_field() }}
-                
-                            <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".6s">
-                                <input type="text" placeholder="Tu nombre" class="form-control" name="name" id="name" required>
+            </div>   
+        </section><!--/#page-header-->
+
+
+        <!-- 
+        ================================================== 
+            Contact Section Start
+        ================================================== -->
+        <section id="contact-section">
+            <div class="container">
+                <div class="row">
+                @if(Session()->has('flash'))
+                    <div class="alert alert-success alert-dismissible" role="alert">
+                      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      {{ session()->get('flash' )}}
+                  </div>
+                @endif
+                    <div class="col-md-6">
+                        <div class="block">
+                            <h2 class="subtitle wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".3s">Contacta conmigo</h2>
+                            <p class="subtitle-des wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".5s">
+                                Si tienes alguna recomendación, duda o sugerencia no dudes en escribirme, te respondere a la brevedad posible.
+                            </p>
+                            <div class="contact-form">
+                                <form id="contact-form" method="post" action="{{ route('messages') }}" role="form">
+                                    {{ csrf_field() }}
+                        
+                                    <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".6s">
+                                        <input type="text" placeholder="Tu nombre" class="form-control" name="name" id="name" required>
+                                    </div>
+                                    
+                                    <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".8s">
+                                        <input type="email" placeholder="Tu correo electronico" class="form-control" name="email" id="email" required >
+                                    </div>
+                                    
+                                    <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay="1s">
+                                        <input type="text" placeholder="Asunto" class="form-control" name="subject" id="subject" required>
+                                    </div>
+                                    
+                                    <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay="1.2s">
+                                        <textarea rows="6" placeholder="Mensaje" class="form-control" name="body" id="message" required></textarea>    
+                                    </div>
+                                    
+                                    
+                                    <div id="submit" class="wow fadeInDown" data-wow-duration="500ms" data-wow-delay="1.4s">
+                                        <input type="submit" id="contact-submit" class="btn btn-default btn-send" value="Enviar mensaje">
+                                    </div>                      
+                                    
+                                </form>
                             </div>
-                            
-                            <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".8s">
-                                <input type="email" placeholder="Tu correo electronico" class="form-control" name="email" id="email" required >
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                         <div class="map-area">
+                            <h2 class="subtitle  wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".3s">Mi ubicación</h2>
+                            <p class="subtitle-des wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".5s">
+                               
+                                
+                            </p>
+                            <div class="map">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125664.85369217841!2d-64.75427033219786!3d10.178486816180415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2d723eff71306f%3A0x22feb43cef889e75!2sBarcelona%2C+Anzo%C3%A1tegui!5e0!3m2!1ses!2sve!4v1487261928687" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+                                
                             </div>
-                            
-                            <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay="1s">
-                                <input type="text" placeholder="Asunto" class="form-control" name="subject" id="subject" required>
-                            </div>
-                            
-                            <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay="1.2s">
-                                <textarea rows="6" placeholder="Mensaje" class="form-control" name="body" id="message" required></textarea>    
-                            </div>
-                            
-                            
-                            <div id="submit" class="wow fadeInDown" data-wow-duration="500ms" data-wow-delay="1.4s">
-                                <input type="submit" id="contact-submit" class="btn btn-default btn-send" value="Enviar mensaje">
-                            </div>                      
-                        </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="row address-details">
+                    <div class="col-md-3">
+                        <div class="address wow fadeInLeft" data-wow-duration="500ms" data-wow-delay=".3s">
+                            <i class="ion-ios-location-outline"></i>
+                            <p>Venezuela</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="address wow fadeInLeft" data-wow-duration="500ms" data-wow-delay=".5s">
+                            <i class="ion-ios-location-outline"></i>
+                            <p>Sector la chica, calle san carlos  <br>Barcelona, edo. Anzoategui</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="email wow fadeInLeft" data-wow-duration="500ms" data-wow-delay=".7s">
+                            <i class="ion-ios-email-outline"></i>
+                            <p>roberto.figuera.web@gmail.com<br>robertsilkro@hotmail.com</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="phone wow fadeInLeft" data-wow-duration="500ms" data-wow-delay=".9s">
+                            <i class="ion-ios-telephone-outline"></i>
+                            <p>0426-621-34-51<br>0281-277-84-03</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
-                 <div class="map-area">
-                    <h2 class="subtitle  wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".3s">Mi ubicación</h2>
-                    <p class="subtitle-des wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".5s">
-                    </p>
-                    <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125664.85369217841!2d-64.75427033219786!3d10.178486816180415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2d723eff71306f%3A0x22feb43cef889e75!2sBarcelona%2C+Anzo%C3%A1tegui!5e0!3m2!1ses!2sve!4v1487261928687" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row address-details">
-            <div class="col-md-3">
-                <div class="address wow fadeInLeft" data-wow-duration="500ms" data-wow-delay=".3s">
-                    <i class="ion-ios-location-outline"></i>
-                    <p>Venezuela</p>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="address wow fadeInLeft" data-wow-duration="500ms" data-wow-delay=".5s">
-                    <i class="ion-ios-location-outline"></i>
-                    <p>Sector la chica, calle san carlos  <br>Barcelona, edo. Anzoategui</p>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="email wow fadeInLeft" data-wow-duration="500ms" data-wow-delay=".7s">
-                    <i class="ion-ios-email-outline"></i>
-                    <p>roberto.figuera.web@gmail.com<br>robertsilkro@hotmail.com</p>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="phone wow fadeInLeft" data-wow-duration="500ms" data-wow-delay=".9s">
-                    <i class="ion-ios-telephone-outline"></i>
-                    <p>0426-621-34-51<br>0281-277-84-03</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+        </section>
 @stop
