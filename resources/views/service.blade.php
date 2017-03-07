fb403bd46d0869faf19d306c3d9141ba27a815db
Roberto Figuera <roberto.figuera.web@gmail.com>
2017-03-07 13:16:54 -0400

    ultiimos
    cam

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -112,28 +112,14 @@
                             <div class="panel-body">
                                 <p style="text-align: left;" class="subtitle-des wow fadeInUp animated" data-wow-delay=".4s" data-wow-duration="700ms"><br>
                                    Si estas leyendo esta sección es porque :<br><br> - Opcion (1) estas interesado en darte a conocer por alguna actividad que realices y quieras ser reconocido/a por lo que haces y deseas tener un <strong>blog</strong> donde escribir tus ideas, notas, recetas y lo sepa la gente.<br><br> - Opcion (2) Eres emprendedor y quieres comerte al mundo ofreciendo productos unicos y asi comenzar a tener presencia online que se traducirian en mas ventas y trafico para tu negocio.<br><br> - Opcion (3) Tienes una <strong>empresa</strong> y necesitas una <strong>pagina web</strong> para tener mas contacto y cercania con tus clientes o tener mas control y llevar registro de lo que ingresa y lo que no a la misma.<br><br>- Opcion (4) Eres estudiante universitario y necesitas soporte para entregar el proyecto ya finiquitado en los lapsos estipulados por tus asesores o profesores.<br><br> Cualquiera que sea tu opcion estas en el lugar indicado contacta mis servicios de <strong>freelance</strong> para hacer un estudio de la problematica y asi poder brindar la solución que mas se adapte a tu necesidad.
-
-
-
-
-
-
                                </p>
                            </div>
                        </div>
                    </div>
                </div>
            </div>
-
              <div class="col-md-6">
             <div class="section-heading">
-
-
-
-
-
-
-
                     <h2 class="wow fadeInDown fondo-h2" data-wow-delay=".5s">OPTIMIZACION DE TU PAGINA WEB SEO + VENTAS = EXITO</h2><br>
                     <div class="media wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay="300ms">
                         <div class="panel panel-default">
@@ -141,8 +127,6 @@
                                 <p style="text-align: left;padding: 5px;line-height: 1.8em;" class="subtitle-des wow fadeInUp animated" data-wow-delay=".7s" data-wow-duration="700ms"><br>
                                    Si tienes en mente abrir una <strong>tienda online</strong> o que en google te busquen y te encuentren de una manera mas facil o aparezcas sin buscarte :D, debes tener presente que hoy en dia existe mucha gente que te pueden diseñar y hacer una <strong>pagina web</strong> sin muchos conocimientos del tema, pero no de una manera profesional, Optimizarla en <strong>SEO</strong> ya es otro nivel. Alcanzar un buen posicionamiento en resultados de búsqueda lleva su tiempo y dedicacion.<br><br>
                    Tambien debes tener claro lo que es el <strong>SEO</strong> esto es nada mas y nada menos la forma en como <strong>google</strong> y otros buscadores te integran o(indexan) en su buscador, teniendo google el 98% de todas las busquedas diarias a nivel mundial. de manera que para que google te lea y te indexe en buenas posiciones se debe llevar una metodologia, que no se hace de un dia para otro es un conjunto de pasos y tecnicas bien analizadas que permitiran al <strong>freelance</strong> tratar de posicionar la <strong>pagina web</strong> entre las primeras busquedas dependiendo el nicho de mercado al cual te vas a dirigir, esto hace la diferencia entre una buena y bien trabajada<strong> pagina web</strong> a cualquier cosa que te vendan como bueno por alli con texto html sin mucho sentido.<br><br>Como trabajador <strong>freelance</strong> me dedico a usar distintos metodos para llevar a cabo esta acción, Porque de nada sirve el <strong>sitio web</strong> mas hermoso y pintoresco del mundo, si no estas posicionado en google, ya que no vas a aparecer y esto a la final no valdria de mucho.Trabajando de este modo el futuro dueño de la <strong>pagina web</strong> va a sentirse muy satisfecho ya que obtendria grandes beneficios a favor de su marca o negocio : mas visitas, mas ventas, mas reconocimiento, mas interacción y podras llegar a estar encima de tu competencia dependiendo del posicionamiento que estos tengan.
-
-
                     <h2 class="wow fadeInDown fondo-h2" data-wow-delay=".4s">OPTIMIZACION DE TU PAGINA WEB SEO + VENTAS = EXITO</h2><br>
                     <div class="media wow fadeInUp animated" data-wow-duration="400ms" data-wow-delay="200ms">
                         <div class="panel panel-default">
@@ -150,41 +134,26 @@
                                 <p style="text-align: left;padding: 5px;line-height: 1.8em;" class="subtitle-des wow fadeInUp animated" data-wow-delay=".6s" data-wow-duration="600ms"><br>
                                    Si tienes en mente abrir una <strong>tienda online</strong> o que en google te busquen y te encuentren de una manera mas facil o aparezcas sin buscarte :D, debes tener presente que hoy en dia existe mucha gente que te pueden diseñar y hacer una <strong>pagina web</strong> sin muchos conocimientos del tema, pero no de una manera profesional, Optimizarla en <strong>SEO</strong> ya es otro nivel. Alcanzar un buen posicionamiento en resultados de búsqueda lleva su tiempo y dedicacion.<br><br>
                    Tambien debes tener claro lo que es el <strong>SEO</strong> esto es nada mas y nada menos la forma en como <strong>google</strong> y otros buscadores te integran o(indexan) en su buscador, teniendo google el 98% de todas las busquedas diarias a nivel mundial. de manera que para que google te lea y te indexe en buenas posiciones se debe llevar una metodologia, que no se hace de un dia para otro es un conjunto de pasos y tecnicas bien analizadas que permitiran al <strong>freelance</strong> tratar de posicionar la <strong>pagina web</strong> entre las primeras busquedas dependiendo el nicho de mercado al cual te vas a dirigir, esto hace la diferencia entre una buena y bien trabajada<strong> pagina web</strong> a cualquier cosa que te vendan como bueno por alli con texto html sin mucho sentido.<br><br>Como trabajador <strong>freelance</strong> me dedico a usar distintos metodos para llevar a cabo estas acciónes, Porque de nada sirve el <strong>sitio web</strong> mas hermoso del mundo si no estas posicionado en google, esto a la final no valdria de mucho.Trabajando de este modo el futuro dueño de la <strong>pagina web</strong> va a sentirse muy satisfecho ya que obtendria grandes beneficios a favor de su marca o negocio : mas visitas, mas ventas, mas reconocimiento, y mas interacción.
-
-
-
                                </p>
                            </div>
                        </div>
                    </div>
              </div>
          </div></div>
-
          <div class="row">
-
              <div class="col-md-12">
             <div class="section-heading">
-
-
-
-
                     <h2 class="wow fadeInDown fondo-h2" data-wow-delay=".7s">ENFOQUE A LA USABILIDAD Y RAPIDES ( DE CARGA )</h2><br>
                     <div class="media wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay="300ms">
                         <div class="panel panel-default">
                             <div class="panel-body">
                                 <p style="text-align: left;padding: 5px;line-height: 1.8em;" class="subtitle-des wow fadeInUp animated" data-wow-delay=".8s" data-wow-duration="700ms"><br>
-
-
                     <h2 class="wow fadeInDown fondo-h2" data-wow-delay=".5s">ENFOQUE A LA USABILIDAD Y RAPIDES ( DE CARGA )</h2><br>
                     <div class="media wow fadeInUp animated" data-wow-duration="400ms" data-wow-delay="200ms">
                         <div class="panel panel-default">
                             <div class="panel-body">
                                 <p style="text-align: left;padding: 5px;line-height: 1.8em;" class="subtitle-des wow fadeInUp animated" data-wow-delay=".7s" data-wow-duration="600ms"><br>
-
-
-
                                    Estamos en el 2017 y las personas en el mundo se conectan mas al internet desde dispositivos moviles que desde computadoras de escritorio, por esta razon y muchas mas veo necesario siempre realizar la <strong>pagina web </strong>con diseño web adaptativo, que no es mas que la construcción de webs adaptables a cualquier tamaño de pantallas, tanto de computadoras de escritorio como de tablets y móviles, con lo que se busca una experiencia en navegación cómoda y atractiva para el usuario final, y ademas personalizada para cada dispositivo.<br><br>
-
                 Estudios como el reciente de www.pagescoring.com (2017) han comprobado que al menos el 50% de los visitantes esperan que una web, cargue en 3 segundos o menos, y si no carga en este tiempo las personas o futuros clientes se van, Y que el 40% de los visitantes abandonan el sitio si toman mas de 4 segundos en cargar, esto no es lo que queremos y por eso trabajo para ello, queremos que los </strong>usuarios y clientes finales</strong> regresen porque les agrade su velocidad y la forma facil de acceder al contenido de una manera bien intuitiva, gracias a el diseño y a la manera en como se va mostrando la información mas importante de todo el <strong>sitio web</strong>, pues de esto trata la usabilidad, por eso como <strong>freelance</strong> pongo especial enfasis en poner en practica todos estos conceptos tan fundamentales antes de hacer el lanzamiento(deploy) de nuestra aplicación o <strong>pagina web.</strong>
                                </p>
                            </div>
@@ -194,8 +163,6 @@
              </div>
          </div>
      </div>
-
-
         <!-- 
         = 
             Works Section Start
@@ -207,28 +174,14 @@
                     <p style="text-align: center;" class="subtitle-des wow fadeInUp animated" data-wow-delay=".3s" data-wow-duration="500ms">
                        En esta sección muestro algunos de mis proyectos ya terminados y otros que estan por terminar en este caso una tienda en linea y dos portafolios personales.
                     </p>
-
-
                 <div class="row">
                     <div class="col-sm-3">
-
-
-
-
-
-                         <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="0ms">
-
-                         <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="100ms">
-
-
                          <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="0ms">
                          <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="100ms">
-
-
                          <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="0ms">
                          <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="100ms">
-
-
+                         <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="0ms">
+                         <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="100ms">
                     <div class="img-wrapper">
                         <img src="images/portfolio/portafolio-web.jpg" class="img-responsive" alt="Portafolio web Roberto figuera desarrollo web-kaz" >
                         <div class="overlay">
@@ -250,26 +203,13 @@
                     </figcaption>
                 </figure>
                     </div>
-
                     <div class="col-sm-3">
-
-
-
-
-
-                        <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
-
-                        <figure class="wow fadeInLeft animated" data-wow-duration="800ms" data-wow-delay="450ms">
-
-
                         <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
                         <figure class="wow fadeInLeft animated" data-wow-duration="800ms" data-wow-delay="450ms">
-
-
                         <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
                         <figure class="wow fadeInLeft animated" data-wow-duration="800ms" data-wow-delay="450ms">
-
-
+                        <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
+                        <figure class="wow fadeInLeft animated" data-wow-duration="800ms" data-wow-delay="450ms">
                     <div class="img-wrapper">
                         <img src="images/portfolio/portafolio-tienda-online.jpg" class="img-responsive" alt="Tienda online creada por Roberto Figuera desarrollo web-kaz" >
                         <div class="overlay">
@@ -291,26 +231,13 @@
                     </figcaption>
                 </figure>
                     </div>
-
                     <div class="col-sm-3">
-
-
-
-
-
-                        <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
-
-                        <figure class="wow fadeInLeft animated" data-wow-duration="1.1s" data-wow-delay="750ms">
-
-
                         <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
                         <figure class="wow fadeInLeft animated" data-wow-duration="1.1s" data-wow-delay="750ms">
-
-
                         <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
                         <figure class="wow fadeInLeft animated" data-wow-duration="1.1s" data-wow-delay="750ms">
-
-
+                        <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
+                        <figure class="wow fadeInLeft animated" data-wow-duration="1.1s" data-wow-delay="750ms">
                     <div class="img-wrapper">
                         <img src="images/portfolio/portafolio-webkaz-roberto-figuera.png" class="img-responsive" alt="pagina web hecha por Roberto Figuera" >
                         <div class="overlay">
@@ -332,24 +259,11 @@
                     </figcaption>
                 </figure>
                     </div>
-
                     <div class="col-sm-3">
-
-
-
-
-
-                        <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="600ms">
-
-                        <figure class="wow fadeInLeft animated" data-wow-duration="1.4" data-wow-delay="1.1s">
-
-
                         <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="600ms">
                         <figure class="wow fadeInLeft animated" data-wow-duration="1.4" data-wow-delay="1.1s">
-
-
-
-
+                        <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="600ms">
+                        <figure class="wow fadeInLeft animated" data-wow-duration="1.4" data-wow-delay="1.1s">
                     <div class="img-wrapper">
                         <img src="images/portfolio/portafolio-web-paginas-web.jpg" class="img-responsive" alt="" >
                         <div class="overlay">
@@ -371,9 +285,7 @@
                     </figcaption>
                 </figure>
                     </div>
-
                    
-
                 </div>
             </div>
         </section>
@@ -424,9 +336,6 @@
             </div>
         </section>
  --}}
-
-
-
-
-
 @stop
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
