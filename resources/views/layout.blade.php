69463764f11edf5478fd9514575837256f07fcaf
Roberto Figuera <roberto.figuera.web@gmail.com>
2017-03-08 10:00:37 -0400

    subo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -160,7 +160,7 @@ Footer Section Start
 			</ul>
 		</div>
 	</div>
-</footer> <!-- /#footer -->
+</footer> <!-- /#footerrr -->
 <script>
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
